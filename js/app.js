c9ea3b5bef08759cad2d72c239aac36b37dbdc0f
Carlos Jesús Morales pica <carlosjesus.moralespica2000@gmail.com>
2022-11-21 13:25:30 +0100

    Galeria terminada con funcionalidad

--- js/app.js
+++ js/app.js
@@ -4,6 +4,12 @@ const btnBurguer = document.querySelector('.btns__burguer')
 const btnClose = document.querySelector('.btns__close')
 const nav = document.querySelector('.header__nav')
 const navElements = document.querySelectorAll('.nav__li')
+
+const galeriaImg = document.querySelectorAll('.galeria__img')
+const galeria = document.querySelector('.galeria__container')
+const galeriaMuestra = document.querySelector('.galeria__muestra')
+const galeriaClose = document.querySelector('.galeria__close')
+
 // funciones
 
 function abrirMenu(){
@@ -22,6 +28,23 @@ navElements.forEach((li) => {
         btnClose.classList.add('hidden')
         btnBurguer.classList.remove('hidden')
     })
+});
+
+galeriaImg.forEach((imagenSeleccionada) => {
+    imagenSeleccionada.addEventListener('click', ()=>{
+        console.log(imagenSeleccionada);
+        let imagenShow = imagenSeleccionada;
+        imagenShow.classList.add('galeria__seleccionado')
+        galeriaMuestra.insertAdjacentElement("afterbegin", imagenShow)
+        galeriaMuestra.classList.add('activo')
+        
+        galeriaClose.addEventListener("click", ()=>{
+            imagenShow.classList.remove('galeria__seleccionado')
+            galeria.appendChild(imagenShow)
+            galeriaMuestra.classList.remove('activo')
+        })
+    })
+    
 });
 // Eventos
 
