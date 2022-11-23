aeccc499cc71eb843dc3c564aad79e561d78482d
Carlos Jesús Morales pica <carlosjesus.moralespica2000@gmail.com>
2022-11-23 11:08:18 +0100

    Finalizado el proyecto para practicar sass

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ const btnBurguer = document.querySelector('.btns__burguer')
 const btnClose = document.querySelector('.btns__close')
 const nav = document.querySelector('.header__nav')
 const navElements = document.querySelectorAll('.nav__li')
+const navLink = document.querySelectorAll('.nav__link')
 
 const galeriaImg = document.querySelectorAll('.galeria__img')
 const galeria = document.querySelector('.galeria__container')
@@ -31,6 +32,18 @@ navElements.forEach((li) => {
     })
 });
 
+// Efecto smooth cuando hacemos click a algun enlace del header
+navLink.forEach((link) => {
+    link.addEventListener('click', (e)=>{
+        e.preventDefault()
+
+        const seccionScroll = e.target.attributes.href.value;
+        const seccion = document.querySelector(seccionScroll);
+        seccion.scrollIntoView({behavior: "smooth"});
+    })
+});
+
+// Mover elementos del dom para ver la imagen mas grande en un container distinto
 galeriaImg.forEach((imagenSeleccionada) => {
     imagenSeleccionada.addEventListener('click', ()=>{
         console.log(imagenSeleccionada);
